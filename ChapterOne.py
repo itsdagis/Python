c5e5e06165ada0781508d93aa85250fa3e2dc9d8
dagi <dagiworkin@gmail.com>
2025-01-23 00:23:28 +0300

    day1

--- ChapterOne.py
+++ ChapterOne.py
@@ -1,2 +1,70 @@
-Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# this is my first python code
+
+print("hello class, how are you doing?")
+
+#data or value
+
+print("Efrem") #varchar : text character
+print(25) #int 
+print("AB101") #varchar 
+print("efrem@test.com") #varchar
+print(10) #int
+print(True) #boolean
+print(type("Efrem")) #to check data type
+
+#data type
+# 1. string any thing under single or double qoute
+# 2. number int (number with no decimal point or fraction)
+# 3. number float and double  (number with decimal point)
+# 4. boolean true or false
+
+Student_Name = "Efrem" #varchar : text character
+Age = 25 #int 
+ID = "AB101" #varchar 
+Email = "efrem@test.com" #varchar
+Grade = 10 #int
+Height = 1.70
+Active = True #boolean
+
+print(Student_Name) #when mentioning variable we don't need qoutation
+print(ID)
+print(Email)
+print(Grade)
+print(Height)
+print(Active)
+
+#variable rule
+#1. Letter capital or smal , numbers $ or _
+#2. Can't start with number 
+#3. Can't any symbol or words that are keywords 
+
+#operators 
+#arthimetic op +/*%
+
+a = 119
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) # divides a by b and shows the remaining number 
+
+#comparison <, >, ==,>=, <=, !=,
+
+print (100 == 200)
+
+#logical operator
+#and, or, not
+# true and true = true
+# true and false = false
+# false and true = false
+# false and false = false
+
+# true or true = true
+# true or false = false
+# false or true = false
+# false or false = false
+
+print(10 < 100 or 10 == 100)
+
+print(200 >= 100)
